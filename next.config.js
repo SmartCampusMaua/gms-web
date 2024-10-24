768b4176004fbdbb7c2657509fd690bf88fc108c
Felipe Nelli Martins <Fn.martins@outlook.com>
2024-10-24 02:26:04 -0300

    Revert "."
    This reverts commit c90b33569df38cd7cda02cff0e403ff407a03145.

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   basePath: '/gms',
   output: "standalone",
-  async redirects() {
-    return [
-      {
-        source: '/gms', // base URL
-        destination: '/gms/reservatorios', // target path
-        permanent: true, // makes the redirect permanent (use 308 status)
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig
--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,5 +1,3 @@
 import { Montserrat } from 'next/font/google';
-import { Outfit } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
-export const outfit = Outfit({ subsets: ['latin'] });
