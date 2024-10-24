c90b33569df38cd7cda02cff0e403ff407a03145
Enrico Saez <enricoricsaez@gmail.com>
2024-10-24 01:51:41 -0300

    .

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   basePath: '/gms',
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: '/gms', // base URL
+        destination: '/gms/reservatorios', // target path
+        permanent: true, // makes the redirect permanent (use 308 status)
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig
